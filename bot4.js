7ce1ddb0eead0117343dc8cdd298690ab15a29c0
concordia3455-cell <concordia3455@gmail.com>
2026-08-11 10:12:59 +0300

    Create bot4.js

---
+++ bot4.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+
+client.on('ready', async () => {
+  console.log(`[BOT 4] ${client.user.tag} aktif!`);
+  client.user.setPresence({ activities: [{ name: 'Apatheon Profesyonel Hizmet', type: ActivityType.Streaming, url: 'https://www.twitch.tv/discord' }], status: 'online' });
+  const guild = await client.guilds.fetch('1230989327958282340');
+  const channel = await guild.channels.fetch('1536592811187638332');
+  if (channel) {
+    joinVoiceChannel({ channelId: channel.id, guildId: guild.id, adapterCreator: guild.voiceAdapterCreator, selfDeaf: true, selfMute: false });
+    console.log(`[BOT 4] Sese bağlandı.`);
+  }
+});
+
+client.login(process.env.BOT_TOKEN_4);
