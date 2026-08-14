3969b75dd86302259e3fd27deac945a666f0875e
concordia3455-cell <concordia3455@gmail.com>
2026-08-14 14:46:00 +0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -11,11 +11,11 @@ const client = new Client({
 });
 
 const GUILD_ID = '1230989327958282340';
-const CHANNEL_ID = '1536592754828902500'; // Kanal 2
+const CHANNEL_ID = '1536592813586522112'; // Welcome 2 Kanalı
 
 client.on('ready', async () => {
   console.log(`[BAŞARILI] Bot 2 giriş yaptı: ${client.user.tag}`);
-  
+
   try {
     const guild = await client.guilds.fetch(GUILD_ID);
     const channel = await guild.channels.fetch(CHANNEL_ID);
@@ -38,6 +38,8 @@ client.on('ready', async () => {
   }
 });
 
+console.log("[BOT 2] Discord'a bağlanma isteği gönderiliyor...");
+
 client.login(process.env.BOT_TOKEN_2)
-  .then(() => console.log("[BAŞARILI] Bot 2 token doğrulandı, Discord'a bağlanıyor..."))
-  .catch((err) => console.error("[CRITICAL HATA - BOT 2 LOGIN]:", err.message));
+  .then(() => console.log("[BOT 2] Login isteği iletildi!"))
+  .catch((err) => console.error("[BOT 2 LOGIN HATASI]:", err.message));
